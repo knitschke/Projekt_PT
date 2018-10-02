9b646eab690ebf43106b23d4b9c534b836e6c9fe
marekW27 <42146077+marekW27@users.noreply.github.com>
2018-10-02 21:57:55 +0200

    2

---
+++ project_pt/project_pt/project_pt/Admin-PI.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_pt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            log f = new log();
+            f.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Functions.tcp("pic");
+            pictures f = new pictures();
+            f.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Functions.tcp("vid");
+            video f = new video();
+            f.Show();
+            this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Functions.tcp("start");
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Functions.tcp("time");
+            time f = new time();
+            f.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ project_pt/project_pt/project_pt/log.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_pt
+{
+    public partial class log : Form
+    {
+        public log()
+        {
+            InitializeComponent();
+        }
+
+        private void log_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            Functions.ip = textBox1.Text;
+            Functions.pwd = textBox2.Text;
+            if (Functions.pwd == "12345") {
+                Functions.tcp("x");
+                Form1 f = new Form1();
+                f.Show();
+                this.Hide();
+            }
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
